84e2e62bcca0a0db9f15129cf65d0a8a2c9e7630
Thanh Long <longthanhpham0001@gmail.com>
2026-04-20 21:22:16 +0700

    chore: remove unused HTTPException import from monitor.py

--- app/api/endpoints/system/monitor.py
+++ app/api/endpoints/system/monitor.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 # from starlette.status import HTTP_503_SERVICE_UNAVAILABLE
 from app.core.config import system_settings
 
